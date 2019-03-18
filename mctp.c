fea00575ca9067e8be58c38b9c1fc2b851640975
Kai M. Wetlesen <kmwetles@us.ibm.com>
2019-03-18 16:31:08 -0700

    Adding state machine logic for MCTP

--- mctp.c
+++ mctp.c
@@ -14,6 +14,7 @@
 int MCTP_send_cred(char *from_host, char *username, char *password) {
 }
 
+/*
 int MCTP_recv_cred(void) {
 	if      (strncmp(command, MCTP_CMD_HELO, 4) == 0) {
 	}
@@ -35,6 +36,7 @@ int MCTP_recv_cred(void) {
 		
 	}
 }
+*/
 
 int MCTP_ctlr_recv(char *message, int message_maxlen) {
 	char buffer[MCTP_BUFLEN];
@@ -101,26 +103,140 @@ uint8_t MCTP_ctlr_next_state(uint8_t cur_state, uint32_t inputs) {
 }
 
 /**
- * MCTP Target Next State
+ * MCTP Target Process Command
  *
- * It determines the next state for the target's state machine based
- * on the current state and the input flags. The state machine is a Moore
- * model state machine where inputs are purely for decisional structures
- * and do not affect the output.
+ * It processes the current command based on the current state of the
+ * target machine, then advances the state based on the decision structure.
+ * The state machine is a Moore model state machine where the next state
+ * is purely a function of the command and current state.
+ * 
+ * Input:
+ * - State cur_state : The current state of the machine.
+ * - char *cmd       : The command to process.
+ * - char *cmdarg    : Any arguments to the command. Passed to command 
+ *                     processor subroutines if required.
  *
- * cur_state : 8-bit unsigned integer indicative of the current protocol state
+ * Output            : The next state of the machine.:
  *
+ * Note: This function fast-fails at the FAST FAIL point.
  */
-uint8_t MCTP_trgt_next_state(uint8_t cur_state, uint32_t inputs) {
-	switch (cur_state) {
-		case MCTP_TRGT_STATE_CONNECTING:
-			break;
-		case MCTP_TRGT_STATE_READY:
-			break;
-		case MCTP_TRGT_STATE_SHUTDOWN:
-			break;
-		case MCTP_TRGT_STATE_BEGIN_DATA:
-			break;
+State MCTP_trgt_process_cmd(State state, char *cmd, char *cmdarg) {
+	int syncheck;
+	
+	syncheck = 0;
+
+	/* 
+	 * FAST FAIL: Verify that arg is set for all variant commands. 
+	 * Return current state if check fails.
+	 */
+	switch (CMD(cmd)) {
+		case MCTP_CMD_HELO:
+		case MCTP_CMD_AUTH:
+		case MCTP_CMD_VRFY:
+		case MCTP_CMD_CHPW:
+			syncheck = strlen(cmdarg) > 1;
+			if (!syncheck) {
+				fputs(MCTP_MSG_SYNTAXERROR, stdout);
+				return state; /* The syntax is invalid, fast-fail out. */
+			}
+		default:
+			syncheck = 1;
 	}
+	
+	switch (command) {
+		case MCTP_CMD_HELO:
+			state.field.helo_ok = process_helo(cmdarg);
+			if (state.field.helo_ok) {
+				fprintf(stdout, MCTP_MSG_ACK_HELO_F, cmdarg);
+			}
+			else {
+				fprintf(stdout, MCTP_MSG_NACK_HELO_F, cmdarg);
+			}
+			break;
+		case MCTP_CMD_AUTH:
+			if (state.field.helo_ok) {
+				state.field.auth_ok = process_auth(cmdarg);
+				if (state.field.auth_ok) {
+					fputs(MCTP_MSG_ACK_AUTH, stdout);
+				}
+				else {
+					fputs(MCTP_MSG_NACK_AUTH, stdout);
+				}
+			}
+			else {
+				fputs(MCTP_MSG_REQ_HELO, stdout);
+			}
+			break;
+		case MCTP_CMD_VRFY:
+			if (state.field.helo_ok && state.field.auth_ok) {
+				if (process_vrfy(cmdarg)) {
+					fprintf(stdout, MCTP_MSG_ACK_VRFY_F, cmdarg);
+				}
+				else {
+					fprintf(stdout, MCTP_MSG_NACK_VRFY_F, cmdarg);
+				}
+			}
+			else if (state.field.helo_ok) {
+				fputs(MCTP_MSG_REQ_AUTH, stdout);
+			}
+			else {
+				fputs(MCTP_MSG_REQ_HELO, stdout);
+			}
+			break;
+		case MCTP_CMD_CHPW:
+			if (state.field.helo_ok && state.field.auth_ok) {
+				// Reverify the user (just in case)
+				
+				state.field.vrfy_ok = process_vrfy(cmdarg);
+				if (state.field.vrfy_ok) { // Then enable chpw once DATA recvd
+					state.field.chpw_run = 1;
+					fprintf(stdout, MCTP_MSG_ACK_CHPW, cmdarg);
+				}
+				else {
+					fprintf(stdout, MCTP_MSG_NACK_CHPW, cmdarg);
+				}
+			}
+			else if (state.field.helo_ok) {
+				fputs(MCTP_MSG_REQ_AUTH, stdout);
+			}
+			else {
+				fputs(MCTP_MSG_REQ_HELO, stdout);
+			}
+		case MCTP_CMD_DATA:
+			/* If we're here, then everything better be in line! */
+			if (state.field.helo_ok && 
+			    state.field.auth_ok && 
+			    state.field.vrfy_ok && 
+			    state.field.chpw_ok) {
+				// Gather up the password data then change the password!
+			} /* Otherwise provide a narrowed status as to what to fix */
+			else if (state.field.helo_ok && 
+			         state.field.auth_ok && 
+			         state.field.vrfy_ok) {
+				fputs(MCTP_MSG_REQ_CHPW, stdout);
+			}
+			else if (state.field.helo_ok && 
+			         state.field.auth_ok) {
+				fputs(MCTP_MSG_REQ_VRFY, stdout);
+			}
+			else if (state.field.helo_ok) {
+				fputs(MCTP_MSG_REQ_AUTH, stdout);
+			}
+			else {
+				fputs(MCTP_MSG_REQ_HELO, stdout);
+			}
+		case MCTP_CMD_RSET:
+			state.value = 0x0;
+			fputs(MCTP_MSG_RESET, stdout);
+		case MCTP_CMD_NOOP:
+			fputs(MCTP_MSG_NOOP, stdout);
+		case MCTP_CMD_QUIT:
+			state.value = 0x0; /* Reset state machine then close down ops */
+			state.field.closing_down = 1;
+			fputs(MCTP_MSG_SHUTTINGDOWN, stdout);
+		default:
+			fputs(MCTP_MSG_COMMANDUNKNWN, stdout);
+	}
+	return state;
 }
 
--- mctp.h
+++ mctp.h
@@ -2,6 +2,14 @@
 #define MCTP_H
 
 /*
+ * REQUIRED LIBRARIES
+ *
+ * These libraries are required for the proper functioning
+ * of MCTP.
+ */
+#include <stdint.h>
+
+/*
  * CONFIGURABLE ADAPTORS
  *
  * MCTP may be configured with any supported adaptor.
@@ -27,29 +35,57 @@
 #define MCTP_CMDLEN 5 /* Four characters plus null terminator */
 #define MCTP_MSGLEN MCTP_BUFLEN - MCTP_CMDLEN + 2 /* +2 to account for addition */
 
-#define MCTP_CMD_HELO "HELO"
-#define MCTP_CMD_AUTH "AUTH"
-#define MCTP_CMD_VRFY "VRFY"
-#define MCTP_CMD_CHPW "CHPW"
-#define MCTP_CMD_DATA "DATA"
-#define MCTP_CMD_RSET "RSET"
-#define MCTP_CMD_NOOP "NOOP"
-#define MCTP_CMD_QUIT "QUIT"
+#define CMD(cmdtext) ((cmdtext)[0] | (cmdtext)[1] << 8 | (cmdtext)[2] << 16 | (cmdtext)[3] << 24)
 
-#define MCTP_STAT_READY         220
-#define MCTP_STAT_SHUTTINGDOWN  221
-#define MCTP_STAT_COMMANDOKAY   250
-#define MCTP_STAT_BEGINDATA     354
-#define MCTP_STAT_CONTINUEDATA  355
-#define MCTP_STAT_UNAVAILABLE   421
-#define MCTP_STAT_ABORTED       451
-#define MCTP_STAT_COMMANDUNKNWN 500
-#define MCTP_STAT_SYNTAXERROR   501
-#define MCTP_STAT_WRONGSEQUENCE 503
-#define MCTP_STAT_CONTROLREJECT 521
-#define MCTP_STAT_ACCESSDENIED  530
-#define MCTP_STAT_USERUNKNOWN   551
-#define MCTP_STAT_TRANSACTFAIL  554
+#define MCTP_CMDTXT_HELO "HELO"
+#define MCTP_CMDTXT_AUTH "AUTH"
+#define MCTP_CMDTXT_VRFY "VRFY"
+#define MCTP_CMDTXT_CHPW "CHPW"
+#define MCTP_CMDTXT_DATA "DATA"
+#define MCTP_CMDTXT_RSET "RSET"
+#define MCTP_CMDTXT_NOOP "NOOP"
+#define MCTP_CMDTXT_QUIT "QUIT"
+
+#define MCTP_CMD_HELO CMD(MCTP_CMDTXT_HELO)
+#define MCTP_CMD_AUTH CMD(MCTP_CMDTXT_AUTH)
+#define MCTP_CMD_VRFY CMD(MCTP_CMDTXT_VRFY)
+#define MCTP_CMD_CHPW CMD(MCTP_CMDTXT_CHPW)
+#define MCTP_CMD_DATA CMD(MCTP_CMDTXT_DATA)
+#define MCTP_CMD_RSET CMD(MCTP_CMDTXT_RSET)
+#define MCTP_CMD_NOOP CMD(MCTP_CMDTXT_NOOP)
+#define MCTP_CMD_QUIT CMD(MCTP_CMDTXT_QUIT)
+
+#define MCTP_CODE_READY         220
+#define MCTP_CODE_SHUTTINGDOWN  221
+#define MCTP_CODE_COMMANDOKAY   250
+#define MCTP_CODE_BEGINDATA     354
+#define MCTP_CODE_CONTINUEDATA  355
+#define MCTP_CODE_UNAVAILABLE   421
+#define MCTP_CODE_ABORTED       451
+#define MCTP_CODE_COMMANDUNKNWN 500
+#define MCTP_CODE_SYNTAXERROR   501
+#define MCTP_CODE_WRONGSEQUENCE 503
+#define MCTP_CODE_CONTROLREJECT 521
+#define MCTP_CODE_ACCESSDENIED  530
+#define MCTP_CODE_USERUNKNOWN   551
+#define MCTP_CODE_TRANSACTFAIL  554
+
+#define MCTP_STAT_ACK_HELO MCTP_CODE_COMMANDOKAY
+#define MCTP_STAT_NACK_HELO MCTP_CODE_CONTROLREJECT
+
+#define MCTP_STAT_ACK_AUTH MCTP_CODE_COMMANDOKAY
+#define MCTP_STAT_NACK_AUTH MCTP_CODE_ACCESSDENIED
+
+#define MCTP_STAT_ACK_VRFY MCTP_CODE_COMMANDOKAY
+#define MCTP_STAT_NACK_VRFY MCTP_CODE_USERUNKNOWN
+
+#define MCTP_STAT_ACK_CHPW MCTP_CODE_COMMANDOKAY
+#define MCTP_STAT_NACK_CHPW MCTP_CODE_USERUNKNOWN
+
+#define MCTP_STAT_REQ_HELO MCTP_CODE_WRONGSEQUENCE
+#define MCTP_STAT_REQ_AUTH MCTP_CODE_WRONGSEQUENCE
+#define MCTP_STAT_REQ_VRFY MCTP_CODE_WRONGSEQUENCE
+#define MCTP_STAT_REQ_CHPW MCTP_CODE_WRONGSEQUENCE
 
 #define MCTP_MSG_TRANSACTFAIL "Transaction failed"
 
@@ -58,6 +94,20 @@
 #define MCTP_CMDFORM_VARIANT   2 /* CMD ARGUMENT */
 #define MCTP_CMDFORM_STREAMING 3 /* Streaming data header  */
 #define MCTP_CMDFORM_STREAMDAT 4 /* Streaming data element */
+
+typedef struct state_fields {
+	uint32_t helo_ok : 1;
+	uint32_t auth_ok : 1;
+	uint32_t vrfy_ok : 1;
+	uint32_t chpw_run : 1;
+	uint32_t streaming_enabled : 1;
+	uint32_t closing_down : 1;
+} StateFields;
+
+typedef union state {
+	uint32_t value;
+	StateFields field;
+} State;
 
 int MCTP_send_cred(char *from_host, char *username, char *password);
 
